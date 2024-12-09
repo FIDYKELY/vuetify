03cfad28b1c2903c6506304cbb83a00b221d7a10
FIDYFIRE <fidyfire@gmail.com>
2024-12-09 16:54:47 +0300

    bar de navigation

---
+++ src/pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-app>
+    <!-- bar de navigation -->
+    <v-app-bar class="px-3" color="grey-darken-4" flat density="compact">
+      <v-spacer></v-spacer>
+      <v-tabs v-model="selectedTab" centered color="grey-darken-1">
+        <v-tab v-for="link in links" ::key="link">
+        {{ link }}
+        </v-tab>
+      </v-tabs>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+    <!-- Container -->
+     <v-main>
+      <v-container fluid>
+        <v-row>
+          <!-- Left col -->
+          <v-col cols="12" sm="2"></v-col>
+          <!-- Main col -->
+          <v-col cols="12" sm="8">
+            <v-sheet
+              v-if="selectedTab == 0"
+              min-height="70vh"
+              rounded="lg"
+              class="pa-4"
+            >
+          Page 0
+          </v-sheet>
+            <v-sheet
+              v-else
+              min-height="70vh"
+              rounded="lg"
+              class="pa-2 pt-4"
+            >
+          Page 1
+          </v-sheet>
+          </v-col>
+          <!-- Right col -->
+          <v-col cols="12" sm="2"></v-col>
+        </v-row>
+      </v-container>
+     </v-main>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+import { ref, watch, onMounted } from 'vue';
+
+let  selectedTab = ref(0)
+const links = ref(["Dashbord", "About"])
+</script>
